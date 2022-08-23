4c69e8319baab9614c2044b237ab39155f997588
HRagab12 <94136817+HRagab12@users.noreply.github.com>
2022-08-23 15:59:39 -0400

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -24,7 +24,7 @@ exports.SELECTORS = {
         toggleFilterMenu: '#button[aria-label="Search filters"]',
         filterBtnsXp: '//ytd-search-filter-renderer/a/div/yt-formatted-string',
         youtubeVideosSection: 'ytd-item-section-renderer',
-        simplifiedResultHeadline: '#title-container > #title',
+        simplifiedResultHeadline: '#title-container #title',
         youtubeVideosRenderer: 'ytd-video-renderer,ytd-grid-video-renderer', // grid is for channels
         url: 'a[href^="/watch"]',
         videoTitle: '#video-title',
